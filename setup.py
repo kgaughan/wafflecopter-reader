2b8de1b3610be0f2460e4a0bc28fd18e6dea59df
Keith Gaughan <k@stereochro.me>
2017-09-10 15:03:10 +0100

    Peg the dependencies to those available in FreeBSD ports.
    Change-Id: I65540072ecdf00440ec684ba8dc6731dd1fc290f

--- setup.py
+++ setup.py
@@ -26,13 +26,13 @@ setup(
     test_suite='tests.suite',
 
     install_requires=[
-        'docopt',
-        'feedparser',
-        'Flask',
-        'Flask-WTF',
-        'peewee',
-        'wtforms',
-        'wtf-peewee',
+        'click>=2.0',
+        'feedparser==5.2.1',
+        'Flask==0.12.1',
+        'Flask-WTF==0.14.2',
+        'peewee==2.9.2',
+        'wtforms==2.1',
+        'wtf-peewee',  # TODO: Create a FreeBSD port for this.
     ],
 
     classifiers=(
