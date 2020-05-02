3324a64fdb166fb22e6f05ab53096c102982eb94
Keith Gaughan <k@stereochro.me>
2020-05-02 20:12:07 +0100

    Update deps to latest in FreeBSD

--- setup.py
+++ setup.py
@@ -10,40 +10,34 @@ def read(filename):
     Read files relative to this file.
     """
     full_path = os.path.join(os.path.dirname(__file__), filename)
-    with open(full_path, 'r') as fh:
+    with open(full_path, "r") as fh:
         return fh.read()
 
 
 setup(
-    name='wafflecopter',
-    version='0.0.1',
-    description='A delicious feed reader!',
-    long_description=read('README') + "\n\n" + read('ChangeLog'),
-    url='https://github.com/kgaughan/wafflecopter/',
-    license='MIT',
+    name="wafflecopter",
+    version="0.0.1",
+    description="A delicious feed reader!",
+    long_description=read("README") + "\n\n" + read("ChangeLog"),
+    url="https://github.com/kgaughan/wafflecopter/",
+    license="MIT",
     zip_safe=True,
-
-    test_suite='tests.suite',
-
+    test_suite="tests.suite",
     install_requires=[
-        'click>=2.0',
-        'feedparser==5.2.1',
-        'Flask==0.12.1',
-        'Flask-WTF==0.14.2',
-        'peewee==2.9.2',
-        'wtforms==2.1',
-        'wtf-peewee',  # TODO: Create a FreeBSD port for this.
+        "click==7.0",
+        "feedparser==5.2.1",
+        "Flask==1.1.1",
+        "Flask-WTF==0.14.3",
+        "peewee==3.8.0",
+        "wtforms==2.1",
+        "wtf-peewee==3.0.0",
     ],
-
     classifiers=(
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ),
-
-    author='Keith Gaughan',
-    author_email='k@stereochro.me',
+    author="Keith Gaughan",
+    author_email="k@stereochro.me",
 )
